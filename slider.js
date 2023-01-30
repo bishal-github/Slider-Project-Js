d45b33d444fa142f1a3b95e6bb92f6c354577f96
bishal-github <shreesbishal44@gmail.com>
2023-01-31 03:44:07 +0545

    adding sliding effect to the both left and right arrow

--- slider.js
+++ slider.js
@@ -2,15 +2,42 @@ const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const slider = document.querySelector('.slider');
 const images = document.querySelectorAll('.image');
+const bottom = document.querySelector('bottom');
 
 let slideNumber = 1;
+const  length = images.length;
+
+
+const nextSlide = () =>{
+ slider.style.transform = `translateX(-${slideNumber*600}px)`;
+    slideNumber++;
+}
+
+const prevSlide = () =>{
+    slider.style.transform = `translateX(-${(slideNumber-2)*600}px)`;
+        slideNumber--;
+}
+
+const getFirstSlide = () =>{
+    slider.style.transform = `translateX(0px)`;
+        slideNumber = 1;
+}
+
+const getLastSlide = () =>{
+    slider.style.transform = `translateX(-${(length -1)*600}px)`;
+        slideNumber = length;
+}
 
 right.addEventListener('click', ()=>{
-    if(slideNumber < images.length){
-        slider.style.transform = `translateX(-${slideNumber*600}px)`;
-    slideNumber++;
-    }else{
-        slider.style.transform = `translateX(0px)`;
-        slideNumber = 1;
-    }
+    slideNumber < length ? nextSlide() : getFirstSlide();
 })
+
+left.addEventListener('click', ()=>{
+    slideNumber > 1 ? prevSlide() : getLastSlide();
+});
+
+for(i=0; i<length; i++){
+    const div = document.createElement('div');
+    div.className = 'button';
+    bottom.appendChild(div);
+}
